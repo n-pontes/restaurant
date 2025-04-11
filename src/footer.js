f39c79d51cdd83a98be59fa076e66705d14908ce
n-pontes <nuno06.np@gmail.com>
2025-04-11 09:12:38 +0200

    Create footer function

---
+++ src/footer.js
@@ -0,0 +1,6 @@
+// footer.js
+export const createFooter = () => {
+    const footerEl = document.createElement('footer');
+    footerEl.textContent = 'Copyright © 2021 n-pontes';
+    return footerEl; // Return the footer structure
+};
