aff092ef224433ab3cd9f960471b57babaf3915c
n-pontes <nuno06.np@gmail.com>
2025-04-11 09:05:48 +0200

    Add: div container for the welcome text - It creates the parent index-page, and will appehend to it all the children, such as welcome, tittle, line, menu-small

--- src/index.js
+++ src/index.js
@@ -19,4 +19,41 @@ export const home = () => {
     containerDiv.classList.add('container');
     contentDiv.appendChild(containerDiv);
 
+    // index-page div is now a child of the div content
+    // All the content for the home page goes here
+    const indexPage = document.createElement('div');
+    indexPage.classList.add('index-page');
+    contentDiv.appendChild(indexPage);
+    // Welcome div
+    const welcomeDiv = document.createElement('div');
+    indexPage.appendChild(welcomeDiv);
+    welcomeDiv.classList.add('welcome');
+    let welcomeText = document.createTextNode('Marinheiro');
+    welcomeDiv.appendChild(welcomeText);
+    // Tittle Div
+    const titleDiv = document.createElement('div');
+    indexPage.appendChild(titleDiv);
+    titleDiv.classList.add('title');
+    let titleText = document.createTextNode('Marisqueira');
+    titleDiv.appendChild(titleText);
+
+    // Line Div
+    const lineDiv = document.createElement('div');
+    lineDiv.classList.add('line');
+    indexPage.appendChild(lineDiv);
+
+    // Subtitle Div
+    const subtitleDiv = document.createElement('div');
+    indexPage.appendChild(subtitleDiv);
+    subtitleDiv.classList.add('subtitle');
+    let subtitleText = document.createTextNode('Onde o mar encontra o sabor, desde 1969!');
+    subtitleDiv.appendChild(subtitleText);
+
+    //Creates the menu button
+    const menuButton = document.createElement('button');
+    menuButton.classList.add('menu-small');
+    let menuButtonText = document.createTextNode('CHECK THE MENU');
+    menuButton.appendChild(menuButtonText);
+    indexPage.appendChild(menuButton);
+
 };
